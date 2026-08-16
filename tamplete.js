5d7adeebcb1bb369c7e729c34915ea1c55eb8a74
Nom-an <nomanvi123@gmail.com>
2026-08-16 23:08:53 +0600

    tamplete string uses

---
+++ tamplete.js
@@ -0,0 +1,15 @@
+const poem = `am pata jora jora
+marbo chabuk chorbo ghora`
+
+console.log(poem);
+
+
+const num1 = 10;
+const num2 = 20;
+
+const total = ` the sum of num1 - ${num1} and num2 -${num2} is ${num1+num2} `;
+console.log(total);
+
+const price = 100;
+const discountedPrice = `After 20% discount the price is ${price *0.8}` 
+console.log(discountedPrice);
