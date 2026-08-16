07fe6a4ea2b36892d1cd5b0b24a0d552c667d2c1
Nom-an <nomanvi123@gmail.com>
2026-08-16 22:35:29 +0600

    using default parameter

--- default-parameter.js
+++ default-parameter.js
@@ -1,8 +1,25 @@
-function sum(num1,num2){
+function sum(num1,num2=10){
     let total = num1+num2;
     console.log(total);
 
     return total;
 }
 
-sum(10,20);
+sum(10,50);
+
+function multiply(number1 , number2){
+    const value = number1*number2;
+    return value;
+}
+
+const tata = multiply(10,10);
+
+console.log(tata);
+
+
+function fullName(s1, s2= ''){
+    const name = s1 + ' ' + s2;
+    console.log(name)
+}
+
+fullName('Noman');
