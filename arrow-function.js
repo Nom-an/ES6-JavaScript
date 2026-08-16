bc748523a5f9b17c4d7715d6009fbd2e15d6e60b
Nom-an <nomanvi123@gmail.com>
2026-08-16 23:26:15 +0600

    uses of arrow function

---
+++ arrow-function.js
@@ -0,0 +1,16 @@
+// function declaration by variable 
+
+const sum = function(num1 , num2 ){
+    return num1 + num2;
+}
+
+console.log(sum(101,12))
+
+
+// arrow function 
+const addition = (num1, num2 )=>{
+    console.log(`I'm arrow function `);
+    return num1 + num2;
+}
+
+console.log(addition(10,30));
