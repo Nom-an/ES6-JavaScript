e9d8f017b522fdf0fcb4590575c260790e387104
Nom-an <nomanvi123@gmail.com>
2026-08-18 21:34:51 +0600

    uses of entries

---
+++ entries.js
@@ -0,0 +1,16 @@
+const employee = {
+    name:'Ranib khan',
+    designation: 'developer',
+    salary:'20000',
+    experience: '1',
+    age: '25'
+}
+
+// const keys = Object.keys(employee);
+// console.log(keys)
+
+// const values = Object.values(employee)
+// console.log(values);
+
+const entries = Object.entries(employee)
+console.log(entries);
