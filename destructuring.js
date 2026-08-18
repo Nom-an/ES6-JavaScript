d59b9873a5792b440a4470df6747db6d5ec11a2d
Nom-an <nomanvi123@gmail.com>
2026-08-18 21:20:24 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,11 @@
+const {brand, price} = { brand: 'Iphone', price: 500, color: 'blue'};
+
+console.log(brand,price);
+
+// ---------------------------------------
+const device = {phone: 'samsung', pp: 14000}
+
+ const  {phone,pp, tt=6} = device;
+
+console.log(phone , pp,tt);
+
