6293e146e48925001dd4cc48c7f590d43ffd6760
Nom-an <nomanvi123@gmail.com>
2026-08-19 10:08:54 +0600

    dot and bracket uses for value access of objects

---
+++ dot-bracket.js
@@ -0,0 +1,16 @@
+const student = {
+    name: 'noman',
+    age: 25,
+    1:'2',
+    'home-address' : 'tangail',
+}
+
+
+const studentName = student.name;
+console.log(studentName);
+
+const studentName2 = student[1];
+console.log(studentName2);
+
+const studentName3 = student["home-address"];
+console.log(studentName3);
