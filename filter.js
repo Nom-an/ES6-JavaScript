cfd54b0f4ab86c9925c4f75f45c91a353eda88ef
Nom-an <nomanvi123@gmail.com>
2026-08-22 00:36:51 +0600

    Uses of find

--- filter.js
+++ filter.js
@@ -2,3 +2,6 @@ const number = [12,43,23,12,2,4,5,6];
 
 const evenNumber = number.filter(x=>x % 2 == 0);
 console.log(evenNumber);
+
+const numberGreaterThen20 = number.filter(xx=> xx>20);
+console.log(numberGreaterThen20);
---
+++ find.js
@@ -0,0 +1,4 @@
+const number  = [1,3,4,2,5,32,43];
+
+const numberLessthen5 = number.find(x=>x<5);
+console.log(numberLessthen5);
